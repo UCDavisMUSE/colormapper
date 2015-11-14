ee9db737ee99c006a473588cf9c5bdfab1e347b1
Zachary T. Harmany <zharmany@gmail.com>
2015-11-13 16:51:03 -0800

    Add the ColorButton class that shows the selected color as the button background and allows the user to pick a color.

---
+++ lib/ColorButton.py
@@ -0,0 +1,50 @@
+import wx
+import wx.lib.buttons as buttons
+
+
+# This is the class for the main Colormapper App
+class ColorButtonTestApp(wx.App):
+    def OnInit(self):
+        frame = ColorButtonTestFrame()
+        frame.Show()
+        self.SetTopWindow(frame)
+        return True
+
+
+class ColorButtonTestFrame(wx.Frame):
+    def __init__(self):
+        self.title = "Color Button Test"
+        wx.Frame.__init__(self, None, -1, self.title, size = (400, 400))
+        
+        # Start with a red button
+        color = (255, 0, 0)
+        button = ColorButton(self, color)
+
+
+class ColorButton(wx.lib.buttons.GenButton):
+    def __init__(self, parent, color = (0, 0, 0), size = (30, 30)):
+        wx.lib.buttons.GenButton.__init__(self, parent, -1, size = size)
+        self.SetBackgroundColour(color)
+        self.Bind(wx.EVT_BUTTON, self.OnButtonPress, self)
+
+    def OnButtonPress(self, event):
+        # Set the initial color of the color dialog box to the current button color
+        data = wx.ColourData()
+        data.SetChooseFull(True)
+        currentColor = self.GetBackgroundColour()
+        data.SetColour(wx.Colour(currentColor[0], currentColor[1], currentColor[2]))
+        # Get the new color from the user
+        dlg = wx.ColourDialog(self, data)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.color = dlg.GetColourData().GetColour()[0:3] # Remove Opacity
+            self.SetBackgroundColour(self.color)            
+        dlg.Destroy()
+
+
+# Start the main loop of the ColorButtonTest App
+if __name__ == '__main__':
+    app = ColorButtonTestApp(False)
+    app.MainLoop()
+    
+    
+
