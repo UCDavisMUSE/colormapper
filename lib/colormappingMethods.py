91eaaa19bd1c7015e20281c4349be1ae2dc1cc3c
Zachary T. Harmany <zharmany@gmail.com>
2015-11-20 11:56:52 -0800

    Add a new method that processes an image on a tile-by-tile basis.

--- lib/colormappingMethods.py
+++ lib/colormappingMethods.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 # This file contains various color mapping methods.
 # Each of the methods is coded to take as input a numpy array of type uint8, 
 # along with various parameters to produce an output that is a numpy array of type uint8.
@@ -42,13 +43,13 @@ def learnAffineColorspaceMap(X, Y):
            
     return (A,c)
     
-def applyAffineColorspaceMap(X, A, c, method = 0):
+def applyAffineColorspaceMap(X, A, c, method = 0, tileSize = (64, 64)):
     # Applies the affine colorspace map
     # y = Ax + c
     # to every pixel in the image X
-    X = X.astype(float)
     if method == 0:
         # Done using reshapes and matrix multiplication
+        X = X.astype(float)
         (n1, n2, n3) = X.shape
         X = X.reshape(n1*n2,n3)
         X = np.dot(X,A.transpose()) + np.dot(np.ones((n1*n2,1),float),c.transpose())
@@ -61,6 +62,7 @@ def applyAffineColorspaceMap(X, A, c, method = 0):
 
     elif method == 1:
         # Done using a for loop
+        X = X.astype(float)
         (n1, n2, n3) = X.shape
         Y = np.empty((n1, n2, n3),float)
         for color in range(n3):
@@ -72,6 +74,7 @@ def applyAffineColorspaceMap(X, A, c, method = 0):
 
     elif method == 2:
         # Done using unrolled for loops, always assume 3 colors
+        X = X.astype(float)
         Y = np.empty(X.shape, float)
         Y[:,:,0] = A[0,0]*X[:,:,0] + A[0,1]*X[:,:,1] + A[0,2]*X[:,:,2] + c[0]
         Y[:,:,1] = A[1,0]*X[:,:,0] + A[1,1]*X[:,:,1] + A[1,2]*X[:,:,2] + c[1]
@@ -80,6 +83,40 @@ def applyAffineColorspaceMap(X, A, c, method = 0):
         Y[Y > 255.0] = 255.0
         Y = Y.astype(np.uint8)
         return Y
+        
+    elif method == 3:
+        # Done by processing a small tile at a time
+
+        heightTiles = int(math.floor(1.0*X.shape[0]/tileSize[0]))
+        widthTiles = int(math.floor(1.0*X.shape[1]/tileSize[1]))
+
+        heightRemainder = X.shape[0] % tileSize[0]
+        widthRemainder = X.shape[1] % tileSize[1]
+
+        for widthTile in range(widthTiles + 1):
+            for heightTile in range(heightTiles + 1):
+             
+                if heightTile < heightTiles and widthTile < widthTiles:
+                    X[heightTile*tileSize[0]:(heightTile+1)*tileSize[0], 
+                        widthTile*tileSize[1]:(widthTile+1)*tileSize[1],:] = applyAffineColorspaceMap(
+                        X[heightTile*tileSize[0]:(heightTile+1)*tileSize[0], 
+                        widthTile*tileSize[1]:(widthTile+1)*tileSize[1],:], A, c)
+                elif heightTile < heightTiles and widthTile == widthTiles and widthRemainder != 0:
+                    X[heightTile*tileSize[0]:(heightTile+1)*tileSize[0], 
+                        widthTile*tileSize[1]:,:] = applyAffineColorspaceMap(
+                        X[heightTile*tileSize[0]:(heightTile+1)*tileSize[0], 
+                            widthTile*tileSize[1]:,:], A, c)
+                elif heightTile == heightTiles and widthTile < widthTiles and heightRemainder != 0:
+                    X[heightTile*tileSize[0]:,
+                        widthTile*tileSize[1]:(widthTile+1)*tileSize[1],:] = applyAffineColorspaceMap(
+                        X[heightTile*tileSize[0]:,
+                            widthTile*tileSize[1]:(widthTile+1)*tileSize[1],:], A, c)
+                elif heightTile == heightTiles and widthTile == widthTiles and widthRemainder != 0 and heightRemainder !=0:
+                    X[heightTile*tileSize[0]:, 
+                        widthTile*tileSize[1]:,:] = applyAffineColorspaceMap(
+                        X[heightTile*tileSize[0]:, 
+                            widthTile*tileSize[1]:,:], A, c)
+        return X
         
     else:
         print("Incorrect method parameter.")
