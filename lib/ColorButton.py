f9fa07697a1f6760e5e5b79705e1a571804e9653
Zachary T. Harmany <zharmany@gmail.com>
2016-01-20 12:35:43 -0800

    Added position arguments to ColorButton class.

--- lib/ColorButton.py
+++ lib/ColorButton.py
@@ -22,17 +22,21 @@ class ColorButtonTestFrame(wx.Frame):
 
 
 class ColorButton(wx.lib.buttons.GenButton):
-    def __init__(self, parent, color = (0, 0, 0), size = (30, 30)):
-        wx.lib.buttons.GenButton.__init__(self, parent, -1, size = size)
+    def __init__(self, parent, color = (0, 0, 0), pos = wx.DefaultPosition, 
+        size = (30, 30)):
+        wx.lib.buttons.GenButton.__init__(self, parent, -1, pos = pos, 
+            size = size)
         self.SetBackgroundColour(color)
         self.Bind(wx.EVT_BUTTON, self.OnButtonPress, self)
 
     def OnButtonPress(self, event):
-        # Set the initial color of the color dialog box to the current button color
+        # Set the initial color of the color dialog
+        # box to the current button color
         data = wx.ColourData()
         data.SetChooseFull(True)
         currentColor = self.GetBackgroundColour()
-        data.SetColour(wx.Colour(currentColor[0], currentColor[1], currentColor[2]))
+        data.SetColour(
+            wx.Colour(currentColor[0], currentColor[1], currentColor[2]))
         # Get the new color from the user
         dlg = wx.ColourDialog(self, data)
         if dlg.ShowModal() == wx.ID_OK:
