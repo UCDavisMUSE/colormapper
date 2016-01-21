0c3ebf608f15e7a7e9bd01943db6c799544a3020
Zachary T. Harmany <zharmany@gmail.com>
2016-01-21 15:48:21 -0800

    Increase gain

--- lib/RemixPanel.py
+++ lib/RemixPanel.py
@@ -8,21 +8,21 @@ class RemixPanel(wx.Panel):
     backgroundSpectrum = (99, 69, 86)
     backgroundThresh = 0
     backgroundGain = 1.0
-    backgroundGainSetting = 25
+    backgroundGainSetting = 10
     backgroundGamma = 1.0
     backgroundGammaSetting = 50
     nucleiColor = ( 70,  30, 150)
     nucleiSpectrum = (71, 31, 153)
     nucleiThresh = 0
     nucleiGain = 1.0
-    nucleiGainSetting = 25
+    nucleiGainSetting = 10
     nucleiGamma = 1.0
     nucleiGammaSetting = 50
     remixMode = 0
     
     # 
     recomputeRemix = False
-    gainValues = np.linspace(0,4,101)
+    gainValues = np.linspace(0,10,101)
     gammaValues = np.logspace(-1,1,101)
     
 
