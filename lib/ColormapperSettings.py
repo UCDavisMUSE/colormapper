d5578e144883219a3c1d7decd13f99de5d0df85d
Zachary T. Harmany <zharmany@gmail.com>
2016-01-25 17:42:52 -0800

    New class for colormapper settings, to more easily allow saving and loading of settings.

---
+++ lib/ColormapperSettings.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+class ColormapperSettings:
+    """
+    This class stores settings for the colormapper app, as well as Get and Set
+    methods to adjust the settings.
+    """
+    # Defaults
+    # For Unmixing:
+    unmix = {
+        "backgroundColor": (244, 205, 100),
+        "backgroundSpectrum": (113, 95, 46),
+        "nucleiColor": (228, 250, 166),
+        "nucleiSpectrum": (90, 99, 66),
+        "subtractBackground": False,
+        "subtractBackgroundAmount": 0
+        }
+    # For Remixing:
+    remix = {
+        "backgroundColor": (230, 160, 200),
+        "backgroundSpectrum": (99, 69, 86),
+        "backgroundThresh": 0,
+        "backgroundGain": 1.0,
+        "backgroundGainSetting": 10,
+        "backgroundGamma": 1.0,
+        "backgroundGammaSetting": 50,
+        "nucleiColor": ( 70,  30, 150),
+        "nucleiSpectrum": (71, 31, 153),
+        "nucleiThresh": 0,
+        "nucleiGain": 1.0,
+        "nucleiGainSetting": 10,
+        "nucleiGamma": 1.0,
+        "nucleiGammaSetting": 50,
+        "remixMode": 0,
+        "gainValuesStart": 0,
+        "gainValuesEnd": 10,
+        "gammaValuesStart": -1,
+        "gammaValuesEnd": 1,
+        }
+    
+
+    # Class defaults able to be overwritten when instantiated
+    def __init__(self, unmix = unmix, remix = remix):
+        self.unmix = unmix
+        self.remix = remix
+        
+    # Get Methods
+    def GetUnmixSettings(self): return self.unmix
+    def GetRemixSettings(self): return self.remix
+    def GetAllSettings(self): return (self.GetUnmixSettings, self.GetRemixSettings)
+    # Unmix
+    def GetUnmixBackgroundColor(self): return self.unmix["backgroundColor"]
+    def GetUnmixBackgroundSpectrum(self): return self.unmix["backgroundSpectrum"]
+    def GetUnmixNucleiColor(self): return self.unmix["nucleiColor"]
+    def GetUnmixNucleiSpectrum(self): return self.unmix["nucleiSpectrum"]
+    def GetUnmixSubtractBackground(self): return self.unmix["subtractBackground"]
+    def GetUnmixSubtractBackgroundAmount(self): return self.unmix["subtractBackgroundAmount"]
+    # Remix
+    def GetRemixBackgroundColor(self): return self.remix["backgroundColor"]
+    def GetRemixBackgroundSpectrum(self): return self.remix["backgroundSpectrum"]
+    def GetRemixBackgroundThresh(self): return self.remix["backgroundThresh"]
+    def GetRemixBackgroundGain(self): return self.remix["backgroundGain"]
+    def GetRemixBackgroundGainSetting(self): return self.remix["backgroundGainSetting"]
+    def GetRemixBackgroundGamma(self): return self.remix["backgroundGamma"]
+    def GetRemixBackgroundGammaSetting(self): return self.remix["backgroundGammaSetting"]
+    def GetRemixNucleiColor(self): return self.remix["nucleiColor"]
+    def GetRemixNucleiSpectrum(self): return self.remix["nucleiSpectrum"]
+    def GetRemixNucleiThresh(self): return self.remix["nucleiThresh"]
+    def GetRemixNucleiGain(self): return self.remix["nucleiGain"]
+    def GetRemixNucleiGainSetting(self): return self.remix["nucleiGainSetting"]
+    def GetRemixNucleiGamma(self): return self.remix["nucleiGamma"]
+    def GetRemixNucleiGammaSetting(self): return self.remix["nucleiGammaSetting"]
+    def GetRemixRemixMode(self): return self.remix["remixMode"]
+    def GetRemixGainValuesStart(self): return self.remix["gainValuesStart"]
+    def GetRemixGainValuesEnd(self): return self.remix["gainValuesEnd"]
+    def GetRemixGammaValuesStart(self): return self.remix["gammaValuesStart"]
+    def GetRemixGammaValuesEnd(self): return self.remix["gammaValuesEnd"]
+  
+    # Set Methods
+    # Note: I have only written methods for variables that should be set
+    # in the interface. Derived attributes will not have (at least for now)
+    # public Set methods.
+    def SetUnmixBackgroundColor(self, color):
+        self.unmix["backgroundColor"] = color
+        self.unmix["backgroundSpectrum"] = self.__NormalizeSpectrum(color)
+
+    def SetUnmixNucleiColor(self, color):
+        self.unmix["nucleiColor"] = color
+        if GetUnmixSubtractBackground():
+            color = self.__SubtractBackground(color, 
+                self.GetUnmixBackgroundColor(), 
+                self.GetUnmixSubtractBackgroundAmount())
+        self.unmix["nucleiColor"] = self.__NormalizeSpectrum(color)
+            
+        
+    # Private Methods
+    def __NormalizeSpectrum(self, color):
+        """
+        Returns a normalized spectrum of the color in the tuple color.
+        
+        Note that, due to rounding, the result may not always add to 255,
+        so do not explicitly rely on this fact.
+        """
+        if color == (0, 0, 0):
+            return color
+        else:
+            intensity = (color[0] + color[1] + color[2])
+            return (
+                int(round(255.0*color[0]/intensity)),
+                int(round(255.0*color[1]/intensity)),
+                int(round(255.0*color[2]/intensity)))
+                
+    def __SubtractBackground(self, foregroundColor, backgroundColor, amount):
+        """
+        Subtracts a certain percentage amount of the background color from
+        the foreground color. Returns a tuple of integers.
+        
+        Note that this is not a spectrum and would need to be normalized.
+        """
+        maxAmount = 255
+        for i in range(3):
+            if backgroundColor[i] != 0:
+                maxAmount = min(maxAmount, 1.0*foregroundColor[i]/backgroundColor[i])
+        return (
+            int(round(foregroundColor[0] - 1.0*(amount/100.0)*maxAmount*backgroundColor[0])),
+            int(round(foregroundColor[1] - 1.0*(amount/100.0)*maxAmount*backgroundColor[1])),
+            int(round(foregroundColor[2] - 1.0*(amount/100.0)*maxAmount*backgroundColor[2])))
