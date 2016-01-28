0a82bbe514884f99323fd471d883f7d6d6f5eaf3
Zachary T. Harmany <zharmany@gmail.com>
2016-01-27 16:39:15 -0800

    Wrap code to only execute if main.

--- colormapper.py
+++ colormapper.py
@@ -1,16 +1,17 @@
-import wx
-from lib.ColormapperFrame import *
+if __name__ == "__main__":
+    import wx
+    from lib.ColormapperFrame import *
 
-# This is the class for the main Colormapper App
-class ColormapperApp(wx.App):
-    def OnInit(self):
-        frame = ColormapperFrame()
-        frame.Show()
-        self.SetTopWindow(frame)
-        return True
+    # This is the class for the main Colormapper App
+    class ColormapperApp(wx.App):
+        def OnInit(self):
+            frame = ColormapperFrame()
+            frame.Show()
+            self.SetTopWindow(frame)
+            return True
 
-# Start the main loop of the Colormapper App        
-if __name__ == '__main__':
-    app = ColormapperApp(False)
-    app.MainLoop()
+    # Start the main loop of the Colormapper App        
+    if __name__ == '__main__':
+        app = ColormapperApp(False)
+        app.MainLoop()
     
